972c6a67a301ce8360b29adec7f5f64aced83072
reravindran <reravindran@deloitte.com.au>
2019-06-14 21:15:29 +1000

    code changes

---
+++ Dockerfiles/entrypoint.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# Inspired from https://github.com/hhcordero/docker-jmeter-client
+# Basically runs jmeter, assuming the PATH is set to point to JMeter bin-dir (see Dockerfile)
+#
+# This script expects the standdard JMeter command parameters.
+#
+set -e
+freeMem=`awk '/MemFree/ { print int($2/1024) }' /proc/meminfo`
+s=$(($freeMem/10*8))
+x=$(($freeMem/10*8))
+n=$(($freeMem/10*2))
+export JVM_ARGS="-Xmn${n}m -Xms${s}m -Xmx${x}m"
+
+echo "START Running Jmeter on `date`"
+echo "JVM_ARGS=${JVM_ARGS}"
+echo "jmeter args=$@"
+
+exec mkdir /tests/
+exec mkdir /tests/${TEST_DIR}/
+exec cp ${TEST_PLAN}.jmx /tests/${TEST_DIR}/
+# Keep entrypoint simple: we must pass the standard JMeter arguments
+jmeter -n \
+    -t "/tests/${TEST_DIR}/${TEST_PLAN}.jmx" \
+    -l "/tests/${TEST_DIR}/${TEST_PLAN}.jtl"
+#exec tail -f jmeter.log
+#    -D "java.rmi.server.hostname=${IP}" \
+#    -D "client.rmi.localport=${RMI_PORT}" \
+#  -R $REMOTE_HOSTS
